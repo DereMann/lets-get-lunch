aad1c343e5d9f15ba027d64caf5c8e88fb90ebc2
DereMann <martinstimpfl@gmx.at>
2019-04-01 14:35:27 +0200

    fixed login tests

--- lets-get-lunch/src/app/login/login.component.spec.ts
+++ lets-get-lunch/src/app/login/login.component.spec.ts
@@ -5,29 +5,67 @@ import { AuthService } from '../services/auth/auth.service';
 import { LoginModule } from './login.module';
 
 import { LoginComponent } from './login.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+class LoginPage {
+  loginBtn: DebugElement;
+  usernameInput: HTMLInputElement;
+  passwordInput: HTMLInputElement;
+
+  addPageElements() {
+    this.loginBtn = fixture.debugElement.query(By.css('button'));
+    this.usernameInput = fixture
+                            .debugElement
+                            .query(By.css('[name=username]'))
+                            .nativeElement;
+    this.passwordInput = fixture
+                            .debugElement
+                            .query(By.css('[name=password'))
+                            .nativeElement;
+  }
+}
 
 class MockAuthService {
   login (credentials) {}
 }
 
 let component: LoginComponent;
+let fixture: ComponentFixture<LoginComponent>;
+let authService: AuthService;
+let router: Router;
+let loginPage: LoginPage;
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ LoginModule ]
+    })
+    .overrideComponent(LoginComponent, {
+      set: {
+        providers: [
+          { provide: AuthService, useClass: MockAuthService },
+          { provide: Router, useClass: RouterStub}
+        ]
+      }
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+
+    loginPage = new LoginPage();
+    authService = fixture.debugElement.injector.get(AuthService);
+    router = fixture.debugElement.injector.get(Router);
+
+    return fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      loginPage.addPageElements();
+    });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
